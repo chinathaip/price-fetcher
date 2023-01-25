47932dfb22454bbd03b9a09805e2a1a3158a8bc0
chinathaip <cartoonabe@gmail.com>
2023-01-25 16:18:24 +0700

    json api server

---
+++ api.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type CoinPriceResponse struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type JSONAPIServer struct {
+	addr string
+	svc  PriceFetcher
+}
+
+func NewJSONAPIServer(addr string, svc PriceFetcher) *JSONAPIServer {
+	return &JSONAPIServer{addr: addr, svc: svc}
+}
+
+func (server *JSONAPIServer) Run() {
+	e := echo.New()
+	e.GET("/", server.handleFetchPrice)
+	e.Start(server.addr)
+}
+
+func (server *JSONAPIServer) handleFetchPrice(c echo.Context) error {
+	coin := c.QueryParam("coin")
+	if coin == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "missing coin param")
+	}
+
+	price, err := server.svc.FetchPrice(context.Background(), coin)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, err.Error())
+	}
+
+	resp := CoinPriceResponse{Name: coin, Price: price}
+
+	return c.JSON(http.StatusOK, resp)
+}
--- main.go
+++ main.go
@@ -1,16 +1,8 @@
 package main
 
-import (
-	"context"
-	"log"
-)
-
 func main() {
 	svc := NewLoggerService(&priceFetcherService{})
 
-	price, err := svc.FetchPrice(context.Background(), "ETH")
-	if err != nil {
-		log.Fatalf("error : %v", err.Error())
-	}
-	log.Println(price)
+	srv := NewJSONAPIServer(":3000", svc)
+	srv.Run()
 }
